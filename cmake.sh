3acb64def5151c116a733e5b7036b9d9c158c158
Catherine Cowie <101484365+xanthe-cat@users.noreply.github.com>
2024-05-14 10:13:38 +1000

    Update cmake.sh
    - removes symbolic links after making executable
    - additional info on/help for build process

--- cmake.sh
+++ cmake.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-# Shell script for creating symbolic links for cofact to enable building the executable
+# Shell script for creating symbolic links for cofact to enable building the executable.
 # This script assumes that the cofact directory is contained within the Prime95 source's
 # gwnum directory, and creates links to the required files in the directories above.
 #
-# The gwnum.a file should be built using the appropriate Makefile in the gwnum directory,
-# e.g.
+# You should have built the gwnum.a library already, using the appropriate Makefile in the
+# gwnum directory:
+# 
+# make -f make64            (Linux 64bit)
+# make -f makebsd64         (FreeBSD 12.3 64bit)
+# make -f makefile          (Linux, FreeBSD 32bit)
+# make -f haiku             (Haiku 32bit)
 # make -f makemac           (macOS)
+# make -f makemsys          (Mingw/Msys)
+# make -f makemw64          (Mingw/MSys 64bit)
+# make -f makeos2           (OS/2 32bit)
+#
+# After making cofact, install to your binary directory e.g., sudo cp cofact /usr/local/bin
+#
+# If you are using an older version of the Prime95 source you may also need:
+# ln -s ../gwnum.ld gwnum.ld
 
 ln -s ../../common.h common.h
 ln -s ../../exponentiate.c exponentiate.c
@@ -22,4 +35,5 @@ ln -s ../../proof_hash.c proof_hash.c
 ln -s ../../sha3.c sha3.c
 ln -s ../../sha3.h sha3.h
 make
+rm *.h exponentiate.c gwnum.a proof_hash.c sha3.c
 ./cofact -h
