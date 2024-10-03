69ad4b101ec407538c184ccc4eb676031a1edfc1
Catherine Cowie <101484365+xanthe-cat@users.noreply.github.com>
2024-10-03 15:38:09 +1000

    Update cmake.sh
    Specify a variable for Prime95 source directory

--- cmake.sh
+++ cmake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Shell script for creating symbolic links for cofact to enable building the executable.
-# This script assumes that the cofact directory is contained within the Prime95 source's
-# gwnum directory, and creates links to the required files in the directories above.
+# This script requires a path to the Prime95 source directory, linking to the required
+# files in the Prime95 source (p95v3019b...) and gwnum directories.
 #
 # You should have built the gwnum.a library already, using the appropriate Makefile in the
-# gwnum directory:
+# gwnum directory (please let us know if cofact works on various flavours of devices!):
 # 
 # make -f make64            (Linux 64bit)
 # make -f makebsd64         (FreeBSD 12.3 64bit)
@@ -19,21 +19,21 @@
 # After making cofact, install to your binary directory e.g., sudo cp cofact /usr/local/bin
 #
 # If you are using an older version of the Prime95 source you may also need:
-# ln -s ../gwnum.ld gwnum.ld
+# ln -s $1/gwnum/gwnum.ld gwnum.ld
 
-ln -s ../../common.h common.h
-ln -s ../../exponentiate.c exponentiate.c
-ln -s ../../exponentiate.h exponentiate.h
-ln -s ../giants.h giants.h
-ln -s ../gwcommon.h gwcommon.h
-ln -s ../gwini.h gwini.h
-ln -s ../gwnum.a gwnum.a
-ln -s ../gwnum.h gwnum.h
-ln -s ../gwthread.h gwthread.h
-ln -s ../gwutil.h gwutil.h
-ln -s ../../proof_hash.c proof_hash.c
-ln -s ../../sha3.c sha3.c
-ln -s ../../sha3.h sha3.h
+ln -s $1/common.h common.h
+ln -s $1/exponentiate.c exponentiate.c
+ln -s $1/exponentiate.h exponentiate.h
+ln -s $1/gwnum/giants.h giants.h
+ln -s $1/gwnum/gwcommon.h gwcommon.h
+ln -s $1/gwnum/gwini.h gwini.h
+ln -s $1/gwnum/gwnum.a gwnum.a
+ln -s $1/gwnum/gwnum.h gwnum.h
+ln -s $1/gwnum/gwthread.h gwthread.h
+ln -s $1/gwnum/gwutil.h gwutil.h
+ln -s $1/proof_hash.c proof_hash.c
+ln -s $1/sha3.c sha3.c
+ln -s $1/sha3.h sha3.h
 make
 rm *.h exponentiate.c gwnum.a proof_hash.c sha3.c
 ./cofact -h
