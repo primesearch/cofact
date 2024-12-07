8673d1c6684e498bc9c81ecc1cf18eb9a56c5203
Catherine Cowie <101484365+xanthe-cat@users.noreply.github.com>
2024-12-08 07:47:53 +1100

    Superfluous if conditional

--- cofact.c
+++ cofact.c
@@ -1477,11 +1477,11 @@ int main (int argc, char **argv) {
                     len = gwtobinary64 (&gwdata, h_gw, r_bin, r_bin_buf_len);
                     mpz_import (H, len, -1, 8, 0, 0, r_bin);
                     if (mpz_cmp (G, H) == 0) {              // Compare G (equation [2]) and H (equation [3])
-                        if (debug || (j % j_progress_inc == 0 && j > 0) || (j % gsq == 0) || j + gerbicz > x) printf ("%10ld (%5.1f%%), %ss/iter: %9.3lf | GEC passed      Wall time = %4d:%02d:%02d (HH:MM:SS)\n", j, 100.0 * j / x, symb, ms_per_iter, wall_hours, wall_mins, wall_secs);
+                        printf ("%10ld (%5.1f%%), %ss/iter: %9.3lf | GEC passed      Wall time = %4d:%02d:%02d (HH:MM:SS)\n", j, 100.0 * j / x, symb, ms_per_iter, wall_hours, wall_mins, wall_secs);
                         gwcopy (&gwdata, g_gw, j_gw);       // Save roll back variables of d(t) and u(t)
                         gwcopy (&gwdata, r_gw, k_gw);
                     } else {
-                        if (debug || (j % j_progress_inc == 0 && j > 0) || (j % gsq == 0) || j + gerbicz > x) printf ("%10ld (%5.1f%%), %ss/iter: %9.3lf | GEC rollback    Wall time = %4d:%02d:%02d (HH:MM:SS)\n", j, 100.0 * j / x, symb, ms_per_iter, wall_hours, wall_mins, wall_secs);
+                        printf ("%10ld (%5.1f%%), %ss/iter: %9.3lf | GEC rollback    Wall time = %4d:%02d:%02d (HH:MM:SS)\n", j, 100.0 * j / x, symb, ms_per_iter, wall_hours, wall_mins, wall_secs);
                         rollback++;
                         gwcopy (&gwdata, j_gw, g_gw);       // Restore from previously saved rollback point
                         gwcopy (&gwdata, k_gw, r_gw);
